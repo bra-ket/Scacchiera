c9e5c3faf07eff55479e6c51f3250054293fc5bc
Massimiliano Lincetto <m.lincetto@gmail.com>
2012-09-26 23:34:18 +0200

    pulito e commentata Box

--- Box.cpp
+++ Box.cpp
@@ -1,31 +1,34 @@
 #include "Box.h"
 
 Box::Box() {
-    free = true;
+	free = true;
 } // Box()
 
 Box::~Box() {
-    if (!free) delete p;
-}
+	if (!free)
+		delete p;
+} // ~Box()
 
-bool Box::isFree(){
-    return free;
-}
+bool Box::isFree() {
+	return free;
+} // isFree()
 
-Piece * Box::getPiece(){
-    if (!free) return p;
-    else return NULL;
-}
+Piece* Box::getPiece () {
+	if (!free)
+		return p;
+	else
+		return NULL;
+} // getPiece()
 
-void Box::putPiece(Piece* q){
-    p=q;
-    if (q != NULL)
-    	free = false;
-    else
-    	free = true;
-}
+void Box::putPiece(Piece* q) {
+	p = q;
+	if (q != NULL)
+		free = false;
+	else
+		free = true;
+} // putPiece()
 
-void Box::empty(){
-    p=NULL;
-    free=true;
-}
+void Box::empty() {
+	p = NULL;
+	free = true;
+} // emtpy();
--- Box.h
+++ Box.h
@@ -1,16 +1,10 @@
-/*
- * Box.h
- *
- *  Created on: 20/lug/2012
- *      Author: massimiliano
- */
-
 #ifndef BOX_H_
 #define BOX_H_
 
 #include "Piece.h"
 
 class Box {
+
 private:
 	Piece * p;
     bool free;
@@ -18,10 +12,19 @@ private:
 public:
 	Box();
 	virtual ~Box();
+
+	// returns true if the box is free
 	bool isFree();
+
+	// returns the content of the box, if any, otherwise returns
 	Piece * getPiece();
-	void putPiece(Piece* q);
+
+	// puts the piece referenced by q in the box
+	void putPiece(Piece * q);
+
+	// empties the box (without destroying the content)
 	void empty();
-};
+
+}; // Box
 
 #endif /* BOX_H_ */
