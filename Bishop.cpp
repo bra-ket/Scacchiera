b3fc3e0f08f301d7d0291da04e9bf848501064cb
Massimiliano Lincetto <m.lincetto@gmail.com>
2012-09-26 23:08:33 +0200

    Cleaning Bishop.

--- Bishop.cpp
+++ Bishop.cpp
@@ -1,24 +1,19 @@
 #include "Bishop.h"
 
 Bishop::Bishop(player p) {
+	pl = p;
 	type = 'B';
-    pl=p;
-    Delta d;
-    for (int i=0; i<32; i++) possibleMoves.push_back(d);
-     for (int i=1; i<8; i++) {
+	Delta d;
+	for (int i=0; i<32; i++)
+		possibleMoves.push_back(d);
+	for (int i = 1; i < 8; i++) {
 		possibleMoves[4*i].x=i;
-    	possibleMoves[4*i].y=i;
-    	possibleMoves[4*i+1].x=i;
-    	possibleMoves[4*i+1].y=-i;
+		possibleMoves[4*i].y=i;
+		possibleMoves[4*i+1].x=i;
+		possibleMoves[4*i+1].y=-i;
 		possibleMoves[4*i+2].x=-i;
-    	possibleMoves[4*i+2].y=-i;
-    	possibleMoves[4*i+3].x=-i;
-    	possibleMoves[4*i+3].y=+i;
-		}
-}
-
-Bishop::~Bishop() {
-	// TODO Auto-generated destructor stub
-}
-
-
+		possibleMoves[4*i+2].y=-i;
+		possibleMoves[4*i+3].x=-i;
+		possibleMoves[4*i+3].y=+i;
+	} // for
+} // Bishop()
--- Bishop.h
+++ Bishop.h
@@ -1,10 +1,3 @@
-/*
- * Bishop.h
- *
- *  Created on: 20/lug/2012
- *      Author: massimiliano
- */
-
 #ifndef BISHOP_H_
 #define BISHOP_H_
 
@@ -12,10 +5,9 @@
 #include <stdlib.h>
 
 class Bishop: public Piece {
-      
 public:
 	Bishop(player p);
 	virtual ~Bishop();
-};
+}; // Bishop
 
 #endif /* BISHOP_H_ */
